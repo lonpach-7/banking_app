392b5aab41652ad301c7463776648f49be72740b
lonpach7 <124077670+lonpach-7@users.noreply.github.com>
2024-12-11 07:40:00 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/authentication_screen.dart';
+import 'pages/authentication_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
